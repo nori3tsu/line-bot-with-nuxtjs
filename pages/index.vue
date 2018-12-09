93d10a0e8e69c05a38f28b6173f7e07d959e2fef
nori3tsu <tugend.licht@gmail.com>
2018-12-09 11:51:12 +0900

    LIFFの初期表示画面の実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,36 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        line-bot-with-nuxtjs
-      </h1>
-      <h2 class="subtitle">
-        My polished Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <div class="container has-text-centered">
+    <div class="column is-4 is-offset-4">
+      <p class="subtitle has-text-grey">
+        Nuxt.jsで作るLINEアプリ[LIFF]
+      </p>
+      <ul>
+        <li>
+          <p class="subtitle">
+            <nuxt-link to="/send_message">
+              メッセージ送信
+            </nuxt-link>
+          </p>
+        </li>
+        <li>
+          <p class="subtitle">
+            <nuxt-link to="/show_profile">
+              プロフィール表示
+            </nuxt-link>
+          </p>
+        </li>
+      </ul>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  // LIFFの初期化
+  mounted() {
+    liff.init(function(data) {
+      console.log(data)
+    })
   }
 }
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
